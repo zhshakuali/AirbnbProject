1c35839272b88f1cf01c6c27cc4865fc7d177701
zhshakuali <zhansayashakuali200309@gmail.com>
2025-06-20 19:07:23 +0500

    Initial Commit

---
+++ AirbnbProject/AirbnbProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbProjectApp.swift
+//  AirbnbProject
+//
+//  Created by Жансая Шакуали on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
